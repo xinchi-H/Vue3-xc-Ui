06f496d889e448b96af1bcc562393490693abb7a
huxc <455908283@qq.com>
2021-12-27 22:34:59 +0800

    🎉 🎉 🎉

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div>hi</div>
+  <xc />
 </template>
 
 <script>
+import Xc from './components/xc.vue';
 
 export default {
   name: 'App',
+  components: {
+    Xc,
+  }
 }
 </script>
