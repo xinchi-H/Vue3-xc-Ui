baef9d9e566b1ffe5b04df0bb0564e4f15dc1359
xc-work <455908283@qq.com>
2022-03-18 18:06:31 +0800

    📦  SwitchDome

---
+++ src/components/SwitchDome.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { inject, Ref } from "vue";
+
+export default {
+  setup() {
+    const menuVisible = inject<Ref<boolean>>('menuVisible')
+    return {menuVisible}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import './index.scss'
 import {createWebHashHistory, createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
+import SwitchDemo from './component/SwitchDemo.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Home},
-    {path: '/doc', component: Doc},
+    { path: '/', component: Home },
+    {
+      path: '/doc',
+      component: Doc,
+      children: [
+        {
+          path: 'switch',
+          component: SwitchDemo,
+        }
+      ],
+    },
   ]
 })
 
