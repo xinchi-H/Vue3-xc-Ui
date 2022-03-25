61b3e914a086b4e8dafacdca377176d91489dde0
xc-work <455908283@qq.com>
2022-03-25 17:36:01 +0800

    📦 初始化button

--- src/components/ButtonDome.vue
+++ src/components/ButtonDome.vue
@@ -1,11 +1,21 @@
 <template>
 <div>
   Button 示例
+  <h1>示例</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </div>
 </template>
 
 <script lang="ts">
+import buttonVue from "../lib/button.vue";
 
+ export default {
+   components: {
+     Button: buttonVue
+   }
+ }
 </script>
 
 <style lang="scss" scoped>
