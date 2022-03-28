ee775b3c465ac2abf9cdd84937b578159161952f
xc-work <455908283@qq.com>
2022-03-28 17:23:19 +0800

    💄 size和level属性

--- src/components/ButtonDome.vue
+++ src/components/ButtonDome.vue
@@ -1,12 +1,46 @@
 <template>
   <div>
     Button 示例
-    <h1>示例</h1>
+    <h1>示例1</h1>
     <div>
       <Button>你好</Button>
       <Button theme="button">你好</Button>
       <Button theme="link">你好</Button>
       <Button theme="text">你好</Button>
+    </div>
+    <h1>示例2</h1>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>普普通通</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">普普通通</Button>
+      <Button theme="link" size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="text" size="big">大大大</Button>
+      <Button theme="text">普普通通</Button>
+      <Button theme="text" size="small">小小小</Button>
+    </div>
+    <h1>示例3</h1>
+    <div>
+      <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link">普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">主要文字按钮</Button>
+        <Button theme="text">普通文字按钮</Button>
+        <Button theme="text" level="danger">危险文字按钮</Button>
+      </div>
     </div>
   </div>
 </template>
--- src/lib/button.vue
+++ src/lib/button.vue
@@ -1,20 +1,40 @@
 <template>
   <button
     class="xc-button"
-    :class="`xc-theme-${theme}`"
+    :class="classes"
   >
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue';
 export default {
   props: {
-    theme: { 
+    theme: {
       type: String,
       default: 'button',
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return [
+        `xc-theme-${theme}`,
+        `xc-size-${size}`,
+        `xc-level-${level}`,
+      ];
+    });
+    return { classes };
+  }
 };
 </script>
 
@@ -24,6 +44,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 
 .xc-button {
   box-sizing: border-box;
@@ -39,13 +60,13 @@ $radius: 4px;
   color: $color;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
   background: white;
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
   }
 
-  &:hover,
-  &:focus {
+  &:hover, &:focus {
     color: $blue;
     border-color: $blue;
   }
@@ -64,7 +85,7 @@ $radius: 4px;
     color: $blue;
     background: none;
 
-    &:hover,&:focus{
+    &:hover, &:focus{
       color: lighten($blue, 10%);
     }
   }
@@ -74,8 +95,72 @@ $radius: 4px;
     box-shadow: none;
     color: inherit;
 
-    &:hover,&:focus{
+    &:hover, &:focus{
       background: darken(white, 5%);;
+    }
+  }
+
+  &.xc-size-big {
+    height: 48px;
+    padding: 0 16px;
+    font-size: 24px;
+  }
+
+  &.xc-size-small {
+    height: 20px;
+    padding: 0 4px;
+    font-size: 12px;
+  }
+
+  &.xc-theme-button {
+    &.xc-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover, &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.xc-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover, &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.xc-theme-link {
+    &.xc-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.xc-theme-text {
+    &.xc-level-main {
+      color: $blue;
+
+      &:hover, &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.xc-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }
