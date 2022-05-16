928e96533717020409cd89694fe42368e93bcaf7
xc-work <455908283@qq.com>
2022-05-16 16:58:26 +0800

    🎨 features 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <h1>xc-UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a href="https://github.com/xinchi-H/Vue3-xc-Ui">
+        <a target="_blank" href="https://github.com/xinchi-H/Vue3-xc-Ui">
           GitHub
         </a>
         <router-link
@@ -17,15 +17,29 @@
     </div>
   </div>
   <div class="features">
-    <svg class="icon">
-      <use xlink:href="#icon-vue"></use>
-    </svg>
-    <svg class="icon">
-      <use xlink:href="#icon-ts"></use>
-    </svg>
-    <svg class="icon">
-      <use xlink:href="#icon-light"></use>
-    </svg>
+    <ul>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>骄傲地使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript</h3>
+        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg class="icon">
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -47,8 +61,7 @@ $color: #007974;
   background: linear-gradient(
     145deg,
     rgba(227, 255, 253, 1) 0%,
-    rgba(183, 233, 230, 1)
-    100%
+    rgba(183, 233, 230, 1) 100%
   );
   clip-path: ellipse(80% 60% at 50% 40%);
 
@@ -79,9 +92,47 @@ $color: #007974;
 }
 
 .features {
-  > svg {
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+
+  @media (min-width: 800px){
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      width: 400px;
+      margin: 16px 0;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text
+      }
+    }
   }
 }
 </style> 
