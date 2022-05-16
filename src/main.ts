e0f6606e211a0c5d2e2290481bfe0af5cb2a622b
huxc <455908283@qq.com>
2022-05-16 20:53:09 +0800

    🎨 引入GitHub样式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './lib/xc.scss'
-import './index.scss'
-import { createApp } from 'vue'
-import { router } from './router'
-import App from './App.vue'
+import './lib/xc.scss';
+import './index.scss';
+import { createApp } from 'vue';
+import { router } from './router';
+import App from './App.vue';
+import 'github-markdown-css';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
  
