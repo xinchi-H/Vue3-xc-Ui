6417f57832713b99f18221bb871683d2ebfc8e62
xc-work <455908283@qq.com>
2022-06-15 17:44:19 +0800

    🎨 小优化，用component展示SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo />
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -17,7 +17,7 @@
     <div class="demo">
       <h2>支持 disabled</h2>
       <div class="demo-component">
-        <Switch2Demo />
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -33,7 +33,6 @@
 /**
  * Switch示例
  */
-import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue'
@@ -44,8 +43,6 @@ export default {
   components: {
     Switch,
     Button,
-    Switch1Demo,
-    Switch2Demo,
   },
   setup() {
     return {
