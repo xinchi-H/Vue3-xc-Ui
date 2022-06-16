84117b9423ba1d2bd0b0ea26d060a09f3ae1fc0b
xc-work <455908283@qq.com>
2022-06-16 16:41:34 +0800

    🎨 封装Demo组件

---
+++ src/components/Demo.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="demo">
+    <h2>{{component.__sourceCodeTitle }}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="codeVisible = !codeVisible">查看代码</Button>
+    </div>
+    <div
+      v-if="codeVisible"
+      class="demo-code"
+    >
+        <pre
+          class="language-html"
+          v-html="html"
+        />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * Demo
+ */
+import { computed, ref, } from "vue";
+import Button from '../lib/Button.vue';
+import 'prismjs'
+import 'prismjs/themes/prism.css';
+const Prism = (window as any).Prism;
+
+export default {
+  name: 'Demo',
+  components: {
+    Button,
+  },
+  props: {
+    component: Object,
+  },
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
+    })
+    const codeVisible = ref(false);
+    return {
+      codeVisible,
+      html,
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+
+  &-component {
+    padding: 16px;
+  }
+
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
