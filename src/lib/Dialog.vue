253335986380b1a810db6dc6f19ca5a452e2b81a
xc-work <455908283@qq.com>
2022-06-24 16:20:13 +0800

    🎨 删除超前的语法

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -71,12 +71,12 @@ export default {
       }
     };
     const ok = () => {
-      if(props.ok?.() !== false) {
+      if(props.ok && props.ok() !== false) {
         close();
       }
     };
     const cancel = () => {
-      props.cancel?.();
+      props.cancel && props.cancel();
       close();
     };
     return {
