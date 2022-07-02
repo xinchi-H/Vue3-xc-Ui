bfa2b26d46434ffbbfdbeb3a71dd651af566a2a3
huxc <455908283@qq.com>
2022-07-02 18:22:02 +0800

    🎨 setup~~

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,16 @@
   <router-view />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { provide, ref } from "vue"
 import { router } from "./router";
 
-export default {
-  name: 'App',
-  setup() {
-    const width = document.documentElement.clientWidth
-    const menuVisible = ref(width >= 500);
-    provide('menuVisible', menuVisible)
-    router.afterEach(() => {
-      if (width <= 500) {
-        menuVisible.value = false;
-      }
-    })
+const width = document.documentElement.clientWidth
+const menuVisible = ref(width >= 500);
+provide('menuVisible', menuVisible)
+router.afterEach(() => {
+  if(width <= 500) {
+    menuVisible.value = false;
   }
-}
+})
 </script>
